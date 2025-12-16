fdfbd82f5b712dd1637f7d018c55ea9825fd8ee5
nibmangit <nibretumengaw@gmail.com>
2025-12-17 03:46:50 +0430

    Initializing the project with neccessary setup and installed dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+ 
+
+function App() { 
+
+  return (
+    <div className="min-h-screen bg-gray-950 font-sans">
+      <div className="text-2xl font-bold tracking-tight text-cyan-400 cursor-pointer">
+        <span className="text-gray-200">Mini</span>Tech
+      </div>
+    </div>
+  )
+}
+
+export default App

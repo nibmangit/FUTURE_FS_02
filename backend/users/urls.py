95e4ca90eb120d489f7e7326e8aed547a97b88ae
nibmangit <nibretumengaw@gmail.com>
2026-05-02 00:38:03 +0300

    add some fake end point to ckeck which db is my backend using

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,9 +5,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from .views import db_check
+
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", MeView.as_view(), name="me"),
+    
+    path('db-check/', db_check),
 ]
